69af72708d7e9d1a0ef5bebc2f7f91dc5a742f00
felipe-miyai <felipe_miyai@yahoo.com.br>
2015-04-18 13:23:24 -0300

    18042015

--- app/src/main/java/com/example/felipe/desenvolvimentomobile/CameraActivity.java
+++ app/src/main/java/com/example/felipe/desenvolvimentomobile/CameraActivity.java
@@ -10,6 +10,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,19 +26,26 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.example.felipe.desenvolvimentomobile.R.id.camera_preview;
 
 public class CameraActivity extends Activity {
 
+    private  byte[] __data;
     private Camera cameraObject;
     private ShowCamera showCamera;
     private ImageView pic;
     public static Camera isCameraAvailiable(){
         Camera object = null;
         try {
-            object = Camera.open(0);
+            object = Camera.open();
         }
         catch (Exception e){
             throw new RuntimeException("TESTE");
@@ -52,6 +61,7 @@ public class CameraActivity extends Activity {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
 
+                __data = data;
                 Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
                 if (bitmap == null) {
                     Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
@@ -81,5 +91,94 @@ public class CameraActivity extends Activity {
     }
 
 
+    //Salva as fotinhas
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    public void onPictureTaken(View view) {
+        byte[] data = __data;
+
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.i("felipe ", e.getMessage());
+            } catch (IOException e) {
+                Log.i("felipe ", e.getMessage());
+            }
+        }
+
+
+
+
+
+    public void botaoExibirNovaTela(View view) {
+
+        Intent intent = new Intent();
+        intent.setClass(CameraActivity.this,
+                MainActivity.class);
+
+        startActivity(intent);
+
+        finish();
+    }
+
+    public void botaoExibirNovaTela2(View view) {
+
+        Intent intent = new Intent();
+        intent.setClass(CameraActivity.this,
+                CameraActivity.class);
+
+        startActivity(intent);
+
+        finish();
+    }
 
 }
--- app/src/main/java/com/example/felipe/desenvolvimentomobile/MainActivity.java
+++ app/src/main/java/com/example/felipe/desenvolvimentomobile/MainActivity.java
@@ -71,4 +71,6 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
