88d655bdae1b4d3132d8d39a6fbabba5f7c82e48
Murilo Mattos <murilomatt@gmail.com>
2015-05-11 21:41:38 -0300

    implementado a persistencia das imagens

--- app/src/main/java/com/example/felipe/desenvolvimentomobile/CameraActivity.java
+++ app/src/main/java/com/example/felipe/desenvolvimentomobile/CameraActivity.java
@@ -10,6 +10,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +27,10 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import static com.example.felipe.desenvolvimentomobile.R.id.camera_preview;
@@ -33,53 +40,83 @@ public class CameraActivity extends Activity {
     private Camera cameraObject;
     private ShowCamera showCamera;
     private ImageView pic;
-    public static Camera isCameraAvailiable(){
-        Camera object = null;
-        try {
-            object = Camera.open(0);
-        }
-        catch (Exception e){
-            throw new RuntimeException("TESTE");
-        }
-        return object;
-    }
+    RelativeLayout preview;
 
-    private Camera.PictureCallback capturedIt;
-
-    {
-        capturedIt = new Camera.PictureCallback() {
-
-            @Override
-            public void onPictureTaken(byte[] data, Camera camera) {
-
-                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                if (bitmap == null) {
-                    Toast.makeText(getApplicationContext(), "not taken", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "taken", Toast.LENGTH_SHORT).show();
-                }
-                cameraObject.release();
-            }
-        };
-    }
+    private static final String PATH_PICTURE = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/Camera";
+    private static final String PREFIX_PICTURE = "instapobre";
+    private static final String EXTENSION_PICTURE = ".jpeg";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        pic = (ImageView)findViewById(R.id.imageView1);
+        pic = (ImageView) findViewById(R.id.imageView1);
         cameraObject = isCameraAvailiable();
         showCamera = new ShowCamera(this, cameraObject);
-        RelativeLayout preview;
+
+        preparePreview();
+    }
+
+    public Camera isCameraAvailiable() {
+        Camera object = null;
+        try {
+            object = Camera.open(0);
+        } catch (Exception e) {
+            throw new RuntimeException("Ocorreu um erro inesperado tente novamente mais tarde!");
+        }
+        return object;
+    }
+
+    public void snapIt(View view) {
+        cameraObject.takePicture(null, null, capturedIt);
+    }
+
+    private Camera.PictureCallback capturedIt;
+
+    {
+        capturedIt = new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                if (bitmap == null) {
+                    showMessage("Foto não capturada!");
+                } else {
+                    saveImage(bitmap);
+                    renewImage();
+                    showMessage("Foto salva!");
+                }
+            }
+        };
+    }
+
+    private void renewImage() {
+        preview.removeAllViews();
+        preparePreview();
+    }
+
+    private void preparePreview() {
         preview = (RelativeLayout) findViewById(camera_preview);
         preview.addView(showCamera);
     }
 
-    public void snapIt(View view){
-        cameraObject.takePicture(null, null, capturedIt);
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
+    private void saveImage(Bitmap bmp) {
 
-
+        FileOutputStream outStream = null;
+        try {
+            String pictureName = PREFIX_PICTURE + System.currentTimeMillis() + EXTENSION_PICTURE;
+            File iPhoto = new File(PATH_PICTURE, pictureName);
+            outStream = new FileOutputStream(iPhoto);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 70, outStream);
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            bmp.recycle();
+        }
+    }
 }
